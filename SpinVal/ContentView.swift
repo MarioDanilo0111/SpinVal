4a86419bfbd66e7c4472f87166a81c776f3c3b8f
Mario Fernández <nilovinilo1@gmail.com>
2025-08-25 15:38:16 +0200

    chore: add clean Xcode project scaffold into develop

---
+++ SpinVal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpinVal
+//
+//  Created by Mario Fernandez on 2025-08-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpinValTests/SpinValTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpinValTests.swift
+//  SpinValTests
+//
+//  Created by Mario Fernandez on 2025-08-24.
+//
+
+import Testing
+@testable import SpinVal
+
+struct SpinValTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
