7cc6ca464ac2f8777cbcb361dfe35006b9226703
Danilo Fernández <79529572+MarioDanilo0111@users.noreply.github.com>
2025-08-25 15:57:39 +0200

    Sprint 1 baseline: clean Xcode project structure (#2)
    * docs: add MIT license
    
    * chore: .gitattributes for line endings adn Swift diffs
    
    * chore: .gitattributes for line endings adn Swift diffs
    
    * docs: updated README.md
    
    * chore: add clean Xcode project scaffold into develop

---
+++ SpinVal/SpinValApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpinValApp.swift
+//  SpinVal
+//
+//  Created by Mario Fernandez on 2025-08-24.
+//
+
+import SwiftUI
+
+@main
+struct SpinValApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
