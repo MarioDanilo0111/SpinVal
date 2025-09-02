936429c18682baeec8d7299b7d569a4ad1874d74
Danilo Fernández <79529572+MarioDanilo0111@users.noreply.github.com>
2025-09-02 17:39:29 +0200

    Feature/s2 api skeleton (#8)
    * chore(data-layer): create Models + Providers + Networking Skeleton
    
    * feat(models): add Release and Price models (Sprint 2 - Point 1.2)

--- Models/PriceModels.swift
+++ Models/PriceModels.swift
@@ -4,4 +4,33 @@
 //
 //  Created by Mario Fernandez on 2025-08-31.
 //
+import Foundation
+
+public typealias CurrencyCode = String
+
+public struct PriceBand: Codable, Hashable, Sendable  {
+    public let low: Double
+    public let median: Double
+    public let high: Double
+    public let currency: CurrencyCode
+    public let source: String
+    public let lastUpdated: Date
+    
+    public init (
+        low: Double,
+        median: Double,
+        high: Double,
+        currency: CurrencyCode,
+        source: String,
+        lastUpdated: Date,
+        
+    ){
+        self.low = low
+        self.median = median
+        self.high = high
+        self.currency = currency
+        self.source = source
+        self.lastUpdated = lastUpdated
+    }
+}
 
